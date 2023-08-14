265c17e6d57026d2977ca399139d7d3debef7946
Irfan0243 <141995861+Irfan0243@users.noreply.github.com>
2023-08-14 10:13:14 +0530

    Add files via upload

---
+++ Guessing.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.Random;
+class Guessing{
+
+ int number;
+ int inputnumber;
+ int noofGuesses=0;
+
+    public int getNoofGuesses() {
+        return noofGuesses;
+    }
+
+    public void setNoofGuesses(int noofGuesses) {
+        this.noofGuesses = noofGuesses;
+    }
+    Guessing(){
+       Random rand=new Random();
+       this.number= rand.nextInt(100);
+    }
+    void takeUserInput(){
+        System.out.print("guess a number:");
+        Scanner sc=new Scanner(System.in);
+        inputnumber= sc.nextInt();
+    }
+    Boolean isCorrect(){
+        noofGuesses++;
+
+        if(inputnumber==number){
+      System.out.format("your guess is right,it was %d\n You guessed it in %d attempts",number,noofGuesses);
+       return true;
+    } else if (inputnumber<number) {
+            System.out.println("Too Low");
+        } else if (inputnumber>number) {
+            System.out.println("Too high");
+        }
+        return false;
+    }
+    }
+public class Guessing {
+    public static void main (String[] args){
+        Game g= new Game();
+        boolean b=false;
+     while (!b){
+         g.takeUserInput();
+         b=g.isCorrect();
+     }
+    }
+}
+
